ba651a3c203188f2cee9fd84234e7a6f9150cb91
Victor Ruban <ruban.victor@mail.ru>
2020-11-29 04:32:25 +0300

    implemented category adding + editing

--- src/pages/categories_list/index.tsx
+++ src/pages/categories_list/index.tsx
@@ -3,8 +3,26 @@ import { TabPage } from "src/shared/tab_page";
 import { Category } from "src/types";
 import React, { useContext } from "react";
 import { DeleteButton } from "src/shared/delete_button";
-import store from "store";
 import { StoreContext } from "src/shared/store";
+
+function getUpdatedCategories(categories: Category[], newCategory: Category) {
+  // insert a new category if the id equals to 0
+  if (newCategory.id === 0) {
+    const existingIds: number[] = categories.map((c) => c.id);
+    const newId = existingIds.length > 0 ? Math.max(...existingIds) + 1 : 1;
+    const updatedCategories = [...categories, { ...newCategory, id: newId }];
+    return updatedCategories;
+  }
+
+  // update the existing category if the id is not 0
+  const updatedCategories = categories.map((c) => {
+    if (c.id === newCategory.id) {
+      return newCategory;
+    }
+    return c;
+  });
+  return updatedCategories;
+}
 
 export function CategoriesList() {
   const [editedCategory, setEditedCategory] = React.useState<Category | null>(
@@ -84,7 +102,18 @@ export function CategoriesList() {
             Cancel
           </Button>
           <Button
-            onClick={() => setEditedCategory(null)}
+            onClick={() => {
+              if (editedCategory === null) {
+                return;
+              }
+
+              const updatedCategories = getUpdatedCategories(
+                categories,
+                editedCategory
+              );
+              setCategories(updatedCategories);
+              setEditedCategory(null);
+            }}
             className="alert-dialog-button alert-dialog-button--rowfooter"
           >
             Ok
